d4adb73bb32e6f56a53e9665d1d9485a45807439
DaFuqs <dominik.m.schmidt@googlemail.com>
2023-06-11 09:37:04 +0200

    1.20 branch. Works without any changes

--- src/main/java/de/dafuqs/head_in_the_clouds/config/Config.java
+++ src/main/java/de/dafuqs/head_in_the_clouds/config/Config.java
@@ -1,14 +1,12 @@
 package de.dafuqs.head_in_the_clouds.config;
 
 import me.shedaniel.autoconfig.*;
-import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.*;
 
 import java.util.*;
 
 @me.shedaniel.autoconfig.annotation.Config(name = "HeadInTheClouds")
 public class Config implements ConfigData {
-
-	@Comment("")
+	
 	public Map<String, Float> WorldCloudHeights = new HashMap<>();
 
 	@Override
