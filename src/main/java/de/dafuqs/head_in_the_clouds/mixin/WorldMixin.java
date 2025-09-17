3fb18f9398180f58bcfa05f41d86164fff2aad06
DaFuqs <dominik.m.schmidt@googlemail.com>
2025-09-17 09:24:53 +0200

    simplify mixins. Ditch client only classes

--- src/main/java/de/dafuqs/head_in_the_clouds/mixin/WorldMixin.java
+++ src/main/java/de/dafuqs/head_in_the_clouds/mixin/WorldMixin.java
@@ -1,5 +1,7 @@
 package de.dafuqs.head_in_the_clouds.mixin;
 
+import com.llamalad7.mixinextras.injector.*;
+import de.dafuqs.head_in_the_clouds.*;
 import de.dafuqs.head_in_the_clouds.api.*;
 import net.minecraft.util.math.*;
 import net.minecraft.world.*;
@@ -9,7 +11,15 @@ import org.spongepowered.asm.mixin.injection.callback.*;
 
 @Mixin(World.class)
 public abstract class WorldMixin {
-
+    
+    @ModifyReturnValue(method = "getRainGradient(F)F", at = @At("RETURN"))
+    public float clouds$getRainGradient(float original, float tickProgress) {
+        if(original > 0) {
+            return HeadInTheClouds.getRainGradient((World) (Object) this, original);
+        }
+        return original;
+    }
+    
     @Inject(method = "hasRain(Lnet/minecraft/util/math/BlockPos;)Z", at = @At("HEAD"), cancellable = true)
     public void clouds$hasRain(BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
         if(!HeadInTheCloudsAPI.allowRainAtPos((World)(Object) this, pos)) {
